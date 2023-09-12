633690b6f4d1c070beb4dcf32361d71ed69e5376
Marcos Augusto <msaorc@hotmail.com>
2023-09-12 08:13:39 -0300

    Criação das controllers da aplicação

--- lgpd-back/routes/TeacherController.js
+++ lgpd-back/routes/TeacherController.js
@@ -3,4 +3,39 @@ let router = express.router()
 
 import teacherService from '../services/TeacherService.js'
 
+router.post("/addTeacher", async function (req, resp) {
+    const teacherModel = {
+        name: req.body.name
+    }
+
+    const teacher = await teacherService.saveTeacher(teacherModel)
+    return resp.status(200).json(teacher)
+})
+
+router.get("/getAllTeachers", async function (req, res) {
+    const allTeachers = await teacherService.getAllTeachers()
+    return res.status(200).json(allTeachers)
+})
+
+router.get("/teacher/:id", async function (req, res) {
+    const teacher = await teacherService.getTeacherById(req.params.id)
+    return res.status(200).json(teacher)
+})
+
+
+router.delete("/deleteTeacher/:id", async function (req, res) {
+    const teacher = await teacherService.deleteTeacherById(req.params.id)
+    return res.status(200).json(teacher)
+})
+
+
+router.put("/updateTeacher/:id", async function (req, res) {
+    const teacherModel = {
+        name: req.body.name
+    }
+
+    const teacher = await teacherService.updateTeacherById(req.params.id, teacherModel)
+    return res.status(200).json(teacher)
+})
+
 export default router;
