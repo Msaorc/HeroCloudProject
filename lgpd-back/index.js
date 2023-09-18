80c305aa9746090fe864508b2ff367383a74d986
Marcos Augusto <msaorc@hotmail.com>
2023-09-18 07:25:04 -0300

    Adição do pagcote cors

--- lgpd-back/index.js
+++ lgpd-back/index.js
@@ -3,12 +3,14 @@ import { pkg } from 'body-parser'
 import { router } from "./routes/router.js";
 import sequelize from "./utils/database.js";
 import association from "./models/Associations.js"
+import cors from "cors"
 
 const app = express()
 const { json, urlendcoded} = pkg
 
 app.use(json())
 app.use(urlendcoded({ extended: true}))
+app.use(cors())
 
 (async () => {
     try {
